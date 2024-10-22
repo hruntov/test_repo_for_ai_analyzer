dafe3fe1bd4e9614ae3f2cd3aa1a6da25a33e2d6
hruntov <hruntov@outlook.com>
2024-10-22 14:23:48 +0300

    Files for test AI anilyzer

---
+++ bye.py
@@ -0,0 +1,8 @@
+def bye(name: str) -> str:
+    """
+    Returns a bye message for the given name.
+
+    :param name: Name to greet
+    :return: Greeting message
+    """
+    return f"Bye, {name}!"
---
+++ hello.py
@@ -0,0 +1,8 @@
+def greet(name: str) -> str:
+    """
+    Returns a greeting message for the given name.
+
+    :param name: Name to greet
+    :return: Greeting message
+    """
+    return f"Hello, {name}!"
